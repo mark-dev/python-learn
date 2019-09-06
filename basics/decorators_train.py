084895ac184e7b4d9ad5cd9fc9efe9e2ed081019
Mark <perez@prk-a.ru>
2019-09-06 09:30:21 +0300

    decorator examples

--- basics/decorators_train.py
+++ basics/decorators_train.py
@@ -1,3 +1,5 @@
+import time
+
 def type_check(correct_type):
     def check_generator(old_function):
         def new_function(arg):
@@ -21,3 +23,26 @@ def first_letter(word):
 
 print(first_letter('Hello World'))
 first_letter(['Not', 'A', 'String'])
+
+
+def timed(method):
+    def print_timestamps(*positional,**kvargs):
+        before = time.monotonic()
+        val  = method(*positional,**kvargs)
+        after = time.monotonic()
+        print("Method takes {} ".format(str(after-before)))
+        return val
+    return print_timestamps
+
+
+@timed
+def my_business_logic_func(**kvargs):
+    print("business logic func start {}".format(kvargs))
+    time.sleep(2)
+    print("business logic func end")
+    return 'abb'
+
+
+
+ss = my_business_logic_func(next='zalupa')
+print(ss)
